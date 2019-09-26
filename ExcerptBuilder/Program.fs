e636ea3ec197af075b8bb94f31951e7d9f699ba4
Dalto Curvelano Jr <daltojr@gmail.com>
2019-09-26 15:39:25 +0300

    Use crossplatform version of PDFSharp

--- ExcerptBuilder/Program.fs
+++ ExcerptBuilder/Program.fs
@@ -2,12 +2,14 @@
 open PdfSharp.Pdf
 open PdfSharp.Pdf.IO
 open Argu
+open PdfSharp.Drawing
+open PdfSharp.Pdf.Annotations
 
 type CliArguments =
     | First_Page of number:int
     | Last_Page of number:int
     | Original_Path of path:string
-    | Destination_path of path:string
+    | Destination_Path of path:string
 with
     interface IArgParserTemplate with
         member s.Usage =
@@ -15,27 +17,41 @@ with
             | First_Page _ -> "specify a first page"
             | Last_Page _ -> "specify a last page"
             | Original_Path _ -> "specify a original document path"
-            | Destination_path _ -> "specify a result document path"
+            | Destination_Path _ -> "specify a destination document path"
             
-
+            
 [<EntryPoint>]
 let main argv =
     Encoding.RegisterProvider(CodePagesEncodingProvider.Instance)
-    
-    let parser = ArgumentParser.Create<CliArguments>(programName = "chunkbuilder.exe")
-    
+ 
+    let addAnnotations page =
+        let gfx = XGraphics.FromPdfPage(page)
+        let font = new XFont("Verdana", 20.0, XFontStyle.Underline)
+        gfx.DrawString("The first text annotation", font, XBrushes.Black, 30.0, 50.0, XStringFormats.Default);
+        let rect = gfx.Transformer.WorldToDefaultPage(new XRect(new XPoint(30.0, 50.0), new XSize(30.0, 50.0)))
+        let link = PdfLinkAnnotation.CreateWebLink(new PdfRectangle(rect), "http://localhost:3000")
+        page.Annotations.Add(link)
+
+    let parser = ArgumentParser.Create<CliArguments>(programName = "excerptbuilder.exe")
     let options = parser.ParseCommandLine argv
-    let first = options.GetResult First_Page
-    let last = options.GetResult Last_Page
+    
     let originalPath = options.GetResult Original_Path
-    let destinationPath = options.GetResult Destination_path
+    let destinationPath = options.GetResult Destination_Path
     
-    let pdf = PdfReader.Open(originalPath, PdfDocumentOpenMode.Import)
-    let doc = new PdfDocument()
+    let originalPdf = PdfReader.Open(originalPath, PdfDocumentOpenMode.Import)
     
+    let first = options.GetResult First_Page
+    let last = if originalPdf.Pages.Count < options.GetResult(Last_Page) then originalPdf.Pages.Count else options.GetResult(Last_Page)
+    
+    let excerpt = new PdfDocument()
     for i in first .. last do
-        doc.AddPage(pdf.Pages.Item(i)) |> ignore
+        let page = originalPdf.Pages.Item(i - 1)
+        excerpt.AddPage(page) |> ignore
+           
+            
+    addAnnotations(excerpt.Pages.Item(last - 1))
+        
     
-    doc.Save(destinationPath)
+    excerpt.Save(destinationPath)
     
     0 // return an integer exit code
